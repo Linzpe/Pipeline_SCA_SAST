c8c18d34f160bc528291ebb52c17b530601c23fa
Javier Linzoain Pedraza <115028961+Linzpe@users.noreply.github.com>
2024-07-23 10:46:00 +0200

    Create Security_gate.sh

---
+++ Security_gate.sh
@@ -0,0 +1,9 @@
+apt-get update && apt-get install -y jq
+highs=$(jq '[.results[] | select(.issue_severity == "HIGH")] | length' bandit_report.json)
+mediums=$(jq '[.results[] | select(.issue_severity == "MEDIUM")] | length' bandit_report.json)
+echo "Numero de HIGH VULNERABILITIES: $highs"
+echo "\nNumero de MEDIUM VULNERABILITIES: $mediums"
+if [ "$highs" > 3 ]; then
+  echo "Hay más de 3 vulnerabilidades HIGH. Fallando la build."
+  exit 1
+fi
