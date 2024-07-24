dda55f2c3c319d82367163ef67443e1a6f269342
Javier Linzoain Pedraza <115028961+Linzpe@users.noreply.github.com>
2024-07-24 10:46:41 +0200

    Update Security_gate.sh

--- Security_gate.sh
+++ Security_gate.sh
@@ -3,7 +3,7 @@ highs=$(jq '[.results[] | select(.issue_severity == "HIGH")] | length' bandit_re
 mediums=$(jq '[.results[] | select(.issue_severity == "MEDIUM")] | length' bandit_report.json)
 echo "Numero de HIGH VULNERABILITIES: $highs"
 echo "\nNumero de MEDIUM VULNERABILITIES: $mediums"
-if [ "$highs" -gt 3 ]; then
-  echo "Hay más de 3 vulnerabilidades HIGH. Fallando la build."
+if [ "$highs" -gt 1 ]; then
+  echo "Hay más de 1 vulnerabilidades HIGH. Fallando la build."
   exit 1
 fi
